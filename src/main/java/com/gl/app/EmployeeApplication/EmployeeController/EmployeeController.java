d24887034b7b31ba30448100d471abf66b2db589
Mohammad Haque <mohammad.haque@globallogic.com>
2024-06-27 11:25:44 +0530

    initial commit

---
+++ src/main/java/com/gl/app/EmployeeApplication/EmployeeController/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.gl.app.EmployeeApplication.EmployeeController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class EmployeeController {
+
+    @GetMapping("/employees")
+    public String getEmployee()
+    {
+        return "Employee details";
+    }
+
+}
+
